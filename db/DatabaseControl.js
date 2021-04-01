9f43c724e52253dec73634669b82a85bc64a33df
Fasina Oreoluwa <dannyoma75@gmail.com>
2021-04-01 09:58:39 -0700

    +*

--- db/DatabaseControl.js
+++ db/DatabaseControl.js
@@ -69,37 +69,6 @@ const dbMethods = {
             
         }
     },
-    insertRecCount: async (id, bank) => {
-        var q = `UPDATE bvnRecCount SET id = ? WHERE bankCode = ?`;
-        var val = [id, bank];
-        return new Promise((resolve, reject) => {
-            connection.query(q, val, (e, r, f) => {
-                if (e) {
-                    console.log("error inserting:: ", e.message);
-                    reject(e);
-                }
-                else resolve(r);
-            })
-        })
-    },
-    gR: async (bank) => {
-        var q = `SELECT * FROM bvnRecCount WHERE bankCode = ${bank}`;
-        return new Promise((resolve, reject) => {
-            connection.query(q, (e, r, f) => {
-                if (e) reject(e.message);
-                else if (r) {
-                    if (r.length > 0) {
-                        resolve(Number(r[0]["id"]));
-                    }
-                    else resolve(Number(0));
-                }
-                else {
-                    console.log("2else: ", r)
-                    resolve(r);
-                }
-            })
-        });
-    },
     totalBank: async (bank) => {
         var q = `SELECT COUNT(*) FROM bank${bank}`;
         return new Promise((resolve, reject) => {
@@ -115,55 +84,6 @@ const dbMethods = {
             })
         });
     },
-    bvnExists: async (bvn) => {
-        var q = `SELECT * FROM bvnRec WHERE bvn = '${bvn}'`;
-        return new Promise((resolve, reject) => {
-            connection.query(q, (e, r, f) => {
-                if (e) {
-                    console.log("--herree");
-                    //console.log(e.message);
-                    reject(e);
-                }
-                else if (r.length > 0) {
-                    console.log("bvn rec exists");
-                    resolve(true);
-                }
-                else resolve(false);
-            });
-        });
-    },
-    totalRecord: async () => {
-        var q = `SELECT COUNT(*) FROM bvnRec`;
-        return new Promise((resolve, reject) => {
-            connection.query(q, (e, r, f) => {
-                if (e) reject(e.message);
-                else if (r) {
-                    console.log(r)
-                    resolve(r[0][`COUNT(*)`]);
-                }
-                else {
-                    resolve(r);
-                }
-            })
-        });
-    },
-    selectAndGen: async (id, bank) => {
-        var q = `SELECT bvn FROM bank${bank} WHERE id = '${id}'`
-        return new Promise((resolve, reject) => {
-            connection.query(q, (e, r, f) => {
-                if (e) reject(e.message);
-                else if (r.length > 0) {
-                    var bvn = r[0].bvn;
-                    resolve(bvn);
-                }
-                else {
-                    console.log(")(");
-                    console.log(r)
-                    resolve(null);
-                }
-            });
-        })
-    },
     updateBvnRec: async () => {
 
     },
@@ -172,11 +92,10 @@ const dbMethods = {
             id int primary key auto_increment,
             product_name char(30) NOT NULL
             product_description char(30) NOT NULL,
-            procuct_varieties char(30) NOT NULL,
+            procuct_varieties JSON DEFAULT NULL,
             date_uploaded char(30) NOT NULL,
             date_edited char(30) NOT NULL,
-            product_id char(10) NOT NULL,
-            KEY (product_name )
+            KEY (product_name, id)
             )`
         connection.query(q, (err, results, fields) => {
             if (err) console.log(err.message)
